b6c35d72eef53474e3bddf011415fbb53bab5679
2022-tatsuki <119149738+2022-tatsuki@users.noreply.github.com>
2023-01-16 22:56:50 +0900

    Add files via upload

---
+++ userName/src/test/java/com/example/userName/UserNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userName;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
